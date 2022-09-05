653b87285ccd0ae615e4ffc1141d59244ed9779a
tatsutei6 <tatsutei6@gmail.com>
2022-09-05 10:59:26 +0900

    Updates

--- src/common/utils.js
+++
@@ -1,9 +0,0 @@
-
-export const parseJsonByString = (str, defaultValue) => {
-  let returnValue = defaultValue;
-  try {
-    returnValue = JSON.parse(str);
-  } catch (e) {
-  }
-  return returnValue;
-}
